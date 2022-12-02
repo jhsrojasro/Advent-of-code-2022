ec1a7bd1602f9731517a3e7864cf00510b8a7b34
Sebastian Rojas <jhsrojasro@unal.edu.co>
2022-12-02 18:58:32 +0100

    fixed commit :/

--- day1_1.hs
+++
@@ -1,8 +0,0 @@
-import System.IO 
-import Data.List.Split
-
-main = do
-    handle <- openFile "input" ReadMode
-    contents <- hGetContents handle
-    (print . maximum) [sum [read x :: Int | x <- elve] | elve <- map (splitOn "\n") (splitOn "\n\n" contents)]
-    hClose handle
