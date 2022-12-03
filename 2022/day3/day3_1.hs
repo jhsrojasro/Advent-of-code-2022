c43b38aeb45f006a7fb9f9adc075e2aa5e250e26
Sebastian Rojas <jhsrojasro@unal.edu.co>
2022-12-03 10:15:03 +0100

    Day3 puzzle 1

---
+++ 2022/day3/day3_1.hs
@@ -0,0 +1,15 @@
+import System.IO
+import Data.List.Split
+import Data.List
+import Data.Maybe
+main = do
+    handle <- openFile "input" ReadMode
+    contents <- hGetContents handle
+    (print . sum . map (priorityRuckSack . splitAtHalf) . splitOn "\n") contents
+
+
+splitAtHalf rucksack = splitAt (length rucksack `div` 2) rucksack
+
+priorityRuckSack rucksack = priority (head ([ x | x <- fst rucksack, y <- snd rucksack , x == y]))
+
+priority letter =  fromMaybe (-1) (elemIndex letter (['a'..'z'] ++ ['A'..'Z'])) + 1
