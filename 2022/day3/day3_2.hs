7a12b14561f661b35e27aa919fa309c36d4dcedf
Sebastian Rojas <jhsrojasro@unal.edu.co>
2022-12-03 10:30:51 +0100

    Day3 Puzzle 2

---
+++ 2022/day3/day3_2.hs
@@ -0,0 +1,12 @@
+import System.IO
+import Data.List.Split
+import Data.List
+import Data.Maybe
+main = do
+    handle <- openFile "input" ReadMode
+    contents <- hGetContents handle
+    (print . sum . map priorityRuckSack . chunksOf 3 . splitOn "\n") contents
+
+priorityRuckSack rucksack = priority (head ([ x | x <- head rucksack, y <- rucksack !! 1 , z <- rucksack !! 2 , x == y && x == z]))
+
+priority letter =  fromMaybe (-1) (elemIndex letter (['a'..'z'] ++ ['A'..'Z'])) + 1
