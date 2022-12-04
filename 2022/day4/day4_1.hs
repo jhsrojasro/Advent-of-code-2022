c324b7b0aac37241496336cc33eb0d07b97ccdff
Sebastian Rojas <jhsrojasro@unal.edu.co>
2022-12-04 19:28:28 +0100

    more redeable code using operator $ and where clause

--- 2022/day4/day4_1.hs
+++ 2022/day4/day4_1.hs
@@ -5,7 +5,11 @@ import Data.List
 main = do
     handle <- openFile "input" ReadMode
     contents <- hGetContents handle
-    let ranges = [ [[(read . head . head) ranges :: Int, (read . last . head) ranges :: Int] ,[(read . head . last) ranges :: Int, (read . last . last) ranges :: Int]] | ranges <- (map (map (splitOn "-") . splitOn ",") . splitOn "\n") contents]
-    (print . length) [ range | range <- ranges, rangeContained ((head . head) range) ((last . head) range) ((head . last) range) ((last . last) range) ]
+    print $ length $ filter rangeContained ((map (map (splitOn "-") . splitOn ",") . splitOn "\n") contents)
 
-rangeContained x1 x2 y1 y2 = x1 >= y1 && x2 <= y2 || y1 >= x1 && y2 <= x2 
+rangeContained :: [[String]] -> Bool
+rangeContained ranges = x1 >= y1 && x2 <= y2 || y1 >= x1 && y2 <= x2 
+    where x1 = read (head $ head ranges) :: Int
+          x2 = read (last $ head ranges) :: Int
+          y1 = read (head $ last ranges) :: Int
+          y2 = read (last $ last ranges) :: Int
--- 2022/day4/day4_2.hs
+++ 2022/day4/day4_2.hs
@@ -5,7 +5,10 @@ import Data.List
 main = do
     handle <- openFile "input" ReadMode
     contents <- hGetContents handle
-    let ranges = [ [[(read . head . head) ranges :: Int, (read . last . head) ranges :: Int] ,[(read . head . last) ranges :: Int, (read . last . last) ranges :: Int]] | ranges <- (map (map (splitOn "-") . splitOn ",") . splitOn "\n") contents]
-    (print . length) [ range | range <- ranges, rangeOverlap ((head . head) range) ((last . head) range) ((head . last) range) ((last . last) range) ]
+    print $ length $ filter rangeOverlap ((map (map (splitOn "-") . splitOn ",") . splitOn "\n") contents)
 
-rangeOverlap x1 x2 y1 y2 =  x1 `elem` [y1.. y2] || x2 `elem` [y1..y2] || y1 `elem` [x1..x2] || y2 `elem` [x1..x2]
+rangeOverlap ranges =  x1 `elem` [y1.. y2] || x2 `elem` [y1..y2] || y1 `elem` [x1..x2] || y2 `elem` [x1..x2]
+    where x1 = read (head $ head ranges) :: Int
+          x2 = read (last $ head ranges) :: Int
+          y1 = read (head $ last ranges) :: Int
+          y2 = read (last $ last ranges) :: Int
