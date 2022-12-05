d2e7295be34c1f8703db58686ea2c4f2c8903e8c
Sebastian Rojas <jhsrojasro@unal.edu.co>
2022-12-05 21:27:31 +0100

    Day 5 shorter version

--- 2022/day5/day5_2.hs
+++ 2022/day5/day5_2.hs
@@ -7,12 +7,9 @@ main = do
     handle <- openFile "input" ReadMode
     contents <- hGetContents handle
     let initial_state = map (map (chunksOf 4) . splitOn "\n") (init $ splitOn "\n"  $ head $ splitOn "\n\n" contents)
-    let stacks = map strip $ transpose [ [selectIdentifier chunk |  chunks <- stack, chunk <- chunks] | stack <- initial_state ]
+    let stacks = map strip $ transpose [ [chunk !! 1 |  chunks <- stack, chunk <- chunks] | stack <- initial_state ]
     let moves =  [ [read (move!!1) :: Int , (read (move!!3) :: Int) -1 , (read (move!!5) :: Int) -1] | move <- map (splitOn " ") $ splitOn "\n" $ last $ splitOn "\n\n" contents]
     print $ head $ transpose $ foldl updateStacks stacks moves
-
-selectIdentifier :: String -> Char
-selectIdentifier string = if head string == '[' then head $ tail string else ' '
 
 updateStacks :: [String] -> [Int] -> [String]
 updateStacks stacks move = [ selectStack stacks move i | i <- [0..length stacks-1]]
